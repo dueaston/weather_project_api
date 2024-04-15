10b3cffd2ec96bd242a6a7e478ad239dade9c196
Dusty Easton <dueaston@gmail.com>
2024-04-15 12:32:15 -0500

    Corrected config.eager_load errors

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -4,6 +4,6 @@ config.database_configuration_file = Rails.root.join('config', 'database.yml')
 
 config.logger = Logger.new(STDOUT)
 config.log_level = :debug
-
+config.eager_load = false 
 config.action_controller.perform_caching = false
 end
